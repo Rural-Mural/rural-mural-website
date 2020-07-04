11c56ad63728d10747418c90b7d4b5afcd357ff4
Jesse Heady <jesse@heady.name>
2020-07-04 00:46:29 -0400

    More template work; some CSS tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 
+sass.compiler = require('node-sass');
+
 gulp.task('css', function() {
   return gulp.src('./assets/sass/**/*.scss')
-    .pipe(sass({
-      outputStyle: 'compressed'
-    })
+    .pipe(sass({ outputStyle: 'compressed' })
     .on('error', sass.logError))
     .pipe(gulp.dest('./assets/css/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./assets/sass/**/*.scss', gulp.series('css'));
 });
